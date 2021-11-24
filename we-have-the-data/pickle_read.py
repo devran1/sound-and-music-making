882f4a289d7bc41a2f1b11b8b96774ad017116cb
devran1 <40788986+devran1@users.noreply.github.com>
2021-11-24 14:30:59 +0300

    Add files via upload

--- we-have-the-data/pickle_read.py
+++ we-have-the-data/pickle_read.py
@@ -5,14 +5,14 @@ import pickle
 import pandas as pd
 
 #shuffled rigth? not shuffle
-f=open("shuffled.pickle","rb")
-x=pickle.load(f)
+#f=open("shuffled.pickle","rb")
+#x=pickle.load(f)
 #print(x)
 
-y=pd.read_pickle("shuffle.pickle")
+#y=pd.read_pickle("shuffle.pickle")
 #print(y)
 
-y=pd.read_pickle("shuffled.pickle")
+y=pd.read_pickle("shuffled-odum.pickle")
 print(y)
-z=pd.read_pickle("original.pickle")
+z=pd.read_pickle("original-odum.pickle")
 print(z)
