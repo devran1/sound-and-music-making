6387046bce5f8c027e34335e70f766b0373df8a5
devran1 <40788986+devran1@users.noreply.github.com>
2021-11-26 15:19:50 +0300

    Add files via upload

--- we-have-the-data/pickle_read.py
+++ we-have-the-data/pickle_read.py
@@ -14,5 +14,16 @@ import pandas as pd
 
 y=pd.read_pickle("shuffled-odum.pickle")
 print(y)
+#print(y[1][0]) y[0][0] y[1][0]
+
 z=pd.read_pickle("original-odum.pickle")
 print(z)
+
+t=pd.read_pickle("all-original.pickle")
+#print(t)
+
+#s=pd.read_pickle("music-data/original-data/original-astral.wav-.pickle")
+#print(s)
+
+#g=pd.read_pickle("music-data/shuffled-data/shuffled-astral.wav-.pickle")
+#print(g)
